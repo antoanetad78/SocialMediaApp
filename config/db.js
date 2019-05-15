f84b5fe1f1280bed377d174c79d81149eb25005a
antoanetad78 <antoanetad78@gmail.com>
2019-05-15 11:49:43 +0200

    connections to localhost and atlas set up, working, currently in ise is the connection to atlas

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const config = require('config')
+mongoose.set('useNewUrlParser', true);
+const db = config.get('mongoURI')
+
+const connectDB = async ()=> {
+    try {
+        await mongoose.connect(db,{
+            useNewUrlParser:true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        })
+
+        console.log(`MongDB connected ...`);
+        
+    } catch (error) {
+        console.error(error.message)
+        //Exit process with failure
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
-
+const connectDB = require('./config/db')
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
+
+//Connect Database
+connectDB()
 
 app
     .get('/', (req, res)=>res.send('API Running'))
