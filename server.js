7a20ebc552911ee0caad8b0d3617c27925824d6c
antoanetad78 <antoanetad78@gmail.com>
2019-05-15 10:08:34 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+
+app
+    .get('/', (req, res)=>res.send('API Running'))
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Social Media App listening on port ${PORT}`);
+    
+})
