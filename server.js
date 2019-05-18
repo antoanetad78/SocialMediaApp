00364706992759b1794ccef89606e3c13f56aa54
antoanetad78 <antoanetad78@gmail.com>
2019-05-18 12:55:06 +0200

    Profile model/schema created

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app
 // Define routes
 
 app.use('/api/users', require('./routes/api/users'))
-app.use('/api/profile', require('./routes/api/profiles'))
+app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 
