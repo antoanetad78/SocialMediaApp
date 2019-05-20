3b944ad5aadc6112ea0c4a275e95957df040e744
antoanetad78 <antoanetad78@gmail.com>
2019-05-20 21:41:38 +0200

    Added the Delete post. Can only be deleted by its autor.Tested in Postman. Working

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -41,6 +41,74 @@ router.post('/', [auth, [
 
 })
 
+// @route         GET api/posts 
+// @desc          Get all posts
+// @access        Private
 
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({
+            date: -1
+        })
+        return res.json(posts)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
+})
+
+// @route         GET api/posts/:id
+// @desc          Get posts by id
+// @access        Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if (!post) return res.status(404).json({
+            msg: 'Post not found'
+        })
+        return res.json(post)
+    } catch (error) {
+        console.error(error.message)
+        if (error.kind === 'ObjectId') return res.status(400).json({
+            msg: 'Invalid Request. Post not found'
+        })
+
+        res.status(500).send('Server error')
+    }
+})
+
+// @route         DELETE api/posts/:id
+// @desc          Delete a post
+// @access        Private
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) return res.status(404).json({
+            msg: 'Post not found'
+        })
+        if (req.user.id === post.user.toString()) {
+            await Post.findByIdAndRemove(req.params.id)
+            return res.status(200).json({
+                msg: 'Post deleted successfully'
+            })
+        }
+
+        return res.status(401).json({
+            msg: 'Only authors can delete posts'
+        })
+
+
+    } catch (error) {
+        console.error(error.message)
+        if (error.kind === 'ObjectId') return res.status(400).json({
+            msg: 'Invalid Request. Post not found'
+        })
+
+        res.status(500).send('Server error')
+    }
+})
 
 module.exports = router
