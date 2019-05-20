4a4d9dd9891dc1cf03850f39cbdaf3bb5ecc0e91
antoanetad78 <antoanetad78@gmail.com>
2019-05-21 00:06:10 +0200

    Added create and delete Comments. Tested with Postman. working

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -29,7 +29,7 @@ router.post('/', [auth, [
             name: user.name,
             avatar: user.avatar,
             user: req.user.id
-        }).populate('user', ['name', 'avatar'])
+        })
 
         const post = await newPost.save()
         return res.json(post)
@@ -159,6 +159,75 @@ router.put('/unlike/:id', auth, async (req, res) => {
         post.likes.splice(like, 1)
         await post.save()
         return res.json(post.likes)
+
+    } catch (error) {
+        console.error(error.message)
+        if (error.kind === 'ObjectId') return res.status(400).json({
+            msg: 'Invalid Request. Post not found'
+        })
+
+        res.status(500).send('Server error')
+    }
+})
+
+// @route  POST   api/posts/comments/:id
+// @desc          Create a post
+// @access        Private
+router.post('/comments/:id', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        const post = await Post.findById(req.params.id)
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        }
+        post.comments.unshift(newComment)
+        await post.save()
+        return res.json(post.comments)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
+
+
+})
+
+// @route         DELETE api/posts/comments/:post_id/:comment_id
+// @desc          Delete a post
+// @access        Private
+
+router.delete('/comments/:post_id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id)
+        if (!post) return res.status(404).json({
+            msg: 'Post not found'
+        })
+        const comment = post.comments.find(comment => comment.id.toString() === req.params.comment_id)
+        if (!comment) return res.status(404).json({
+            msg: 'Comment not found'
+        })
+
+        if (req.user.id === comment.user.toString()) {
+            const removeIndex = post.comments.map(comment => comment.id.toString() === req.params.comment_id).indexOf()
+            post.comments.splice(removeIndex, 1)
+            await post.save()
+            return res.status(200).json(post.comments)
+        }
+
+        return res.status(401).json({
+            msg: 'You can only delete your own comments!'
+        })
+
 
     } catch (error) {
         console.error(error.message)
