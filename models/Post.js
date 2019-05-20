54a874fe489fa49ec6697e6333bbe5862b8b469c
antoanetad78 <antoanetad78@gmail.com>
2019-05-20 20:05:11 +0200

    Post model created

--- models/Post.js
+++ models/Post.js
@@ -20,5 +20,31 @@ const PostSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: 'user'
         }
-    }]
+    }],
+    comments: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String
+        },
+        avatar: {
+            type: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
 })
+
+module.exports = Post = mongoose.model('post', PostSchema)
