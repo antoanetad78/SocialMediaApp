9c92bd944267452a62e0bea6efc1ebfeda58c570
antoanetad78 <antoanetad78@gmail.com>
2019-05-22 15:10:49 +0200

    Set up front end Sigh Up and Login pages.No redux yet.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
@@ -9,10 +10,12 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 // Init middleware
-app.use(express.json({ extended:false }))
-
+app.use(express.json({
+    extended: false
+}))
+app.use(cors())
 app
-    .get('/', (req, res)=>res.send('API Running'))
+    .get('/', (req, res) => res.send('API Running'))
 
 // Define routes
 
@@ -21,7 +24,7 @@ app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Social Media App listening on port ${PORT}`);
-    
+
 })
