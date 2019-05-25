8043f007bd4b3f409308a8931d74d40c88cf490d
antoanetad78 <antoanetad78@gmail.com>
2019-05-25 13:54:47 +0200

    Added PrivateRoute component to redirect based on authentication status

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routes/PrivateRoute";
+
 //Redux stuff
 import { Provider } from "react-redux";
 import store from "./store";
@@ -31,6 +34,7 @@ function App() {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
